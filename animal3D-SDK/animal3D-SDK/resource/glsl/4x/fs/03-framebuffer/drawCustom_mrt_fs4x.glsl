510c5ed071e4b7496995aead91da4e48a8715dd9
dbucksteincc <dbucksteincc@localhost>
2019-01-20 13:58:16 -0500

    Setting up lab 3 starter, adding resources. MODIFIED: demo project and filters, added new shaders. ADDED: dummy fragment shaders for Phong shading MRT and custom effects MRT.

---
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/03-framebuffer/drawCustom_mrt_fs4x.glsl
@@ -0,0 +1,40 @@
+/*
+	Copyright 2011-2019 Daniel S. Buckstein
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+/*
+	animal3D SDK: Minimal 3D Animation Framework
+	By Daniel S. Buckstein
+	
+	drawCustom_fs4x.glsl
+	Custom effects for MRT.
+*/
+
+#version 410
+
+// ****TO-DO: 
+//	0) copy entirety of other fragment shader to start, or start from scratch: 
+//		-> declare varyings to read from vertex shader
+//		-> *test all varyings by outputting them as color
+//	1) declare at least four render targets
+//	2) implement four custom effects, outputting each one to a render target
+
+out vec4 rtFragColor;
+
+void main()
+{
+	// DUMMY OUTPUT: all fragments are FADED CYAN
+	rtFragColor = vec4(0.5, 1.0, 1.0, 1.0);
+}
