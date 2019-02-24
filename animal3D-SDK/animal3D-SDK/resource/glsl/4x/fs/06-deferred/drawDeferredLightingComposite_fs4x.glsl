ffa9d702d87e5716ae4727e4bf9836810008cf10
zachary.phillips <zachary.phillips@localhost>
2019-02-24 16:53:15 -0500

    defered lighting composite was worked on

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/06-deferred/drawDeferredLightingComposite_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/06-deferred/drawDeferredLightingComposite_fs4x.glsl
@@ -34,11 +34,54 @@
 //	5) compute final Phong shading model (i.e. the final sum)
 
 in vec2 vPassTexcoord;
+in vec4 vPassBiasClipCoord;
 
 layout (location = 0) out vec4 rtFragColor;
 
+uniform sampler2D uImage4;
+uniform sampler2D uImage5;
+uniform sampler2D uImage6;
+uniform sampler2D uImage7;
+
+// (2) declare uniforms for textures (diffuse, specular)
+uniform sampler2D tex_dm[]; // diffuse sample
+uniform sampler2D tex_sm[]; // specular sample
+
+uniform sampler2D tex_atlas_dm[]; // diffuse sample
+uniform sampler2D tex_atlas_sm[]; // specular sample
+
+// (3) temporary values for textures
+vec4 tempTex_dm;
+vec4 tempTex_sm;
+
+vec4 gPosition; // (2)
+vec4 gNormal; // (2)
+vec2 gTexcoord; // (2)
+float gDepth; // (2)
+
 void main()
 {
+	gPosition = texture(uImage4, vPassTexcoord); // (2)
+	gNormal = texture(uImage5, vPassTexcoord); // (2)
+	gTexcoord = texture(uImage6, vPassTexcoord).xy; // (2)
+
+	tempTex_dm = texture(tex_atlas_dm[0], gTexcoord.xy);
+	tempTex_sm = texture(tex_atlas_sm[0], gTexcoord.xy);
+
 	// DUMMY OUTPUT: all fragments are FADED CYAN
-	rtFragColor = vec4(0.5, 1.0, 1.0, 1.0);
+	//rtFragColor = tempTex_dm * tempTex_sm * gPosition;
+
+	//Pretty sure this is wrong
+	vec4 diffuse_DM_atlas_total = (tempTex_dm * vPassBiasClipCoord);
+	vec4 diffuse_SM_atlas_total = (tempTex_sm * vPassBiasClipCoord);
+
+	vec4 total = (diffuse_DM_atlas_total + diffuse_SM_atlas_total) * vec4(.5, .5, .5, 1.0);
+	//total.a = 1;
+
+	rtFragColor = total;
+
+	//diffuse of atlas * diffuse of lighting 
+	//same for specular
+	//add and multiply by ambiant
+	//make sure alpha is the gTexcoord.w
 }
